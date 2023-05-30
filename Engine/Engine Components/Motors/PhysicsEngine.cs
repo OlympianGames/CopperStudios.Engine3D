63ff54669e35b0f15acc2913aaf47560cd90e83d
OlympianGames <xXBUILDERXx1436@outlook.com>
2023-05-29 20:43:44 -0500

    Added PhysicsEngine
    did nothing with it though. this is the start of fucking pain

---
+++ Engine/Engine Components/Motors/PhysicsEngine.cs
@@ -0,0 +1,12 @@
+using CopperStudios.UI;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using System.Text.Json.Serialization;
+
+namespace CopperStudios.Engine;
+
+/// <summary> Internal engine for debugging </summary>
+public static class PhysicsEngine
+{   
+
+}
